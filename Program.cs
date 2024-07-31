0c8623dc1a1e46c10c2c77ef205024e53a3955f3
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-07-31 11:09:02 -0300

    feat: adds new end points & new class

--- Program.cs
+++ Program.cs
@@ -2,5 +2,22 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/user", () => new { name = "Thalles Vicenzo", age = 22 });
+app.MapGet("/AddHeader", (HttpResponse response) =>
+{
+  response.Headers.Append("Test", "Just testing");
+  return "Testing";
+});
+
+app.MapPost("/saveproduct", (Product product) =>
+{
+  return product.Code + " - " + product.Name;
+});
 
 app.Run();
+
+public class Product
+{
+  public string Code { get; set; }
+  public string Name { get; set; }
+}
