484ebd0610e90455375b21300b0f2f2e95b63ba8
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-08-14 15:58:05 -0300

    refactor: organizes project code

--- Program.cs
+++ Program.cs
@@ -1,10 +1,9 @@
 
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<ApplicationDbContext>();
+builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration["Database:SqlServer"]);
 
 var app = builder.Build();
 
@@ -71,74 +70,3 @@ if (app.Environment.IsStaging())
   });
 
 app.Run();
-
-public static class ProductRepository
-{
-  public static List<Product>? Products { get; set; } = Products = [];
-
-  public static void Init(IConfiguration configuration)
-  {
-    var products = configuration.GetSection("Products").Get<List<Product>>();
-    Products = products;
-  }
-
-
-  public static void add(Product product)
-  {
-    if (Products == null)
-      Products?.Add(product);
-  }
-
-  public static Product? getBy(string code)
-  {
-    return Products?.FirstOrDefault(p => p.Code == code);
-  }
-
-  public static void remove(Product product)
-  {
-    Products?.Remove(product);
-  }
-
-
-}
-
-public class Category
-{
-  public int Id { get; set; }
-  public required string Name { get; set; }
-}
-
-public class Tag
-{
-  public int Id { get; set; }
-  public required string Name { get; set; }
-  public int ProductId { get; set; }
-}
-
-public class Product
-{
-  public int Id { get; set; }
-  public required string Code { get; set; }
-  public required string Name { get; set; }
-  public string? Description { get; set; }
-  public required Category Category { get; set; }
-  public required List<Tag> Tags { get; set; }
-}
-
-public class ApplicationDbContext : DbContext
-{
-  public DbSet<Product> Products { get; set; }
-
-  protected override void OnModelCreating(ModelBuilder modelBuilder)
-  {
-    base.OnModelCreating(modelBuilder);
-
-    modelBuilder.Entity<Product>().Property(p => p.Description).HasMaxLength(500).IsRequired(false);
-    modelBuilder.Entity<Product>().Property(p => p.Name).HasMaxLength(120).IsRequired();
-    modelBuilder.Entity<Product>().Property(p => p.Code).HasMaxLength(20).IsRequired();
-  }
-
-  protected override void OnConfiguring(DbContextOptionsBuilder options)
-  => options.UseSqlServer(
-      "Server=localhost;Database=Products;User Id=sa;Password=@Sql2022;MultipleActiveResultSets=true;Encrypt=YES;TrustServerCertificate=YES");
-}
