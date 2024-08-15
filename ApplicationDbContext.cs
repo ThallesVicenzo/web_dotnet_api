9c08ecd9eabf4a84b1e9e22c7dd0f71e5c0931f5
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2024-08-15 14:41:52 -0300

    feat: adds getter and setter to categories

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 public class ApplicationDbContext : DbContext
 {
   public DbSet<Product> Products { get; set; }
+  public DbSet<Category> Categories { get; set; }
 
   public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
@@ -13,5 +14,6 @@ public class ApplicationDbContext : DbContext
     modelBuilder.Entity<Product>().Property(p => p.Description).HasMaxLength(500).IsRequired(false);
     modelBuilder.Entity<Product>().Property(p => p.Name).HasMaxLength(120).IsRequired();
     modelBuilder.Entity<Product>().Property(p => p.Code).HasMaxLength(20).IsRequired();
+    modelBuilder.Entity<Category>().ToTable("Categories");
   }
 }
